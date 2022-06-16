fc6dcb2a3804c149a2fc05393b1488a2c14203c7
karolina węgrzyn <karolinaa.wegrzyn@student.uj.edu.pl>
2022-06-16 09:10:03 +0200

    final

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,72 +1,173 @@
 package com.example.login;
+import androidx.fragment.app.FragmentActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.login.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-    EditText username, password, repassword;
-    Button btnSignUp,btnSignIn;
-    DBUsers myDBUsers;
+import java.util.ArrayList;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    long pause;
+    boolean firstLocation = true;
+    Location oldLocation = null;
+    Location newLocation = null;
+    float distance = 0.0f;
+    Chronometer chronometer;
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        repassword = findViewById(R.id.repassword);
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        btnSignUp = findViewById(R.id.btnRegister);
-        btnSignIn = findViewById(R.id.btnLogIn);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        myDBUsers = new DBUsers(this);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        ArrayList<LatLng> directionsPoints = new ArrayList<>();
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        Button btnS, btnStop;
+        chronometer = findViewById(R.id.chronometerView);
+        chronometer.setFormat("Time: %ms");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        btnS = findViewById(R.id.startRunButton);
+        btnStop = findViewById(R.id.stopRunButton);
+
+        btnS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-                String repass = repassword.getText().toString();
+                if (!running) {
+                    chronometer.setBase(SystemClock.elapsedRealtime() - pause);
+                    chronometer.start();
+                    running = true;
+                }
 
-                if(user.equals("") || pass.equals("") || repass.equals("")){
-                    Toast.makeText(MainActivity.this, "Wypełnij wszystkie pola.", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (pass.equals(repass)) {
-                        Boolean usercheckResult = myDBUsers.checkUsername(user);
-                        if(usercheckResult == false){
-                            Boolean regResult = myDBUsers.insertData(user, pass);
-                            if(regResult == true){
-                                Toast.makeText(MainActivity.this, "Zarejstrowano.", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(MainActivity.this, "Rejestracja nie powiodła się.", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(MainActivity.this, "Taka nazwa użytkownika już istnieje.", Toast.LENGTH_SHORT).show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        try {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+                                @Override
+                                public void onLocationChanged(Location location) {
+                                    if (running) {
+                                        try {
+                                            newLocation = location;
+                                            if (firstLocation) {
+                                                firstLocation = false;
+                                                oldLocation = location;
+                                            } else {
+                                                distance += oldLocation.distanceTo(newLocation);
+                                                ((TextView) findViewById(R.id.textViewDistance)).setText("distance: " + (int) distance + "m");
+
+                                                LatLng myPoss = new LatLng(location.getLatitude(), location.getLongitude());
+                                                directionsPoints.add(myPoss);
+                                                showPolyline(directionsPoints);
+                                                mMap.clear();
+                                                directionsPoints.add(myPoss);
+                                                showPolyline(directionsPoints);
+                                                mMap.addMarker(new MarkerOptions().position(myPoss).title("my location"));
+                                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15.0f));
+                                            }
+                                            oldLocation = newLocation;
+                                        } catch (SecurityException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onProviderDisabled(String provider) {
+                                    // TODO Auto-generated method stub
+                                }
+
+                                @Override
+                                public void onProviderEnabled(String provider) {
+                                    // TODO Auto-generated method stub
+                                }
+
+                                @Override
+                                public void onStatusChanged(String provider, int status,
+                                                            Bundle extras) {
+                                    // TODO Auto-generated method stub
+                                }
+                            });
+                        } catch (SecurityException e) {
+                            e.printStackTrace();
                         }
-
-                    } else {
-                        Toast.makeText(MainActivity.this, "Hasła nie są zgodne.", Toast.LENGTH_SHORT).show();
                     }
+
+                }, 1000);
+            }
+
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (running) {
+                    chronometer.stop();
+                    pause = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    running = false;
                 }
             }
         });
-
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
     }
+
+
+    private void showPolyline(ArrayList<LatLng> points) {
+        if (points.size() < 2)
+            return;
+
+        int ix = 0;
+        LatLng currentPoint = points.get(ix);
+        ArrayList<LatLng> currentSegment = new ArrayList<>();
+        currentSegment.add(currentPoint);
+        ix++;
+
+        while (ix < points.size()) {
+            currentPoint = points.get(ix);
+            currentSegment.add(currentPoint);
+            mMap.addPolyline(new PolylineOptions()
+                    .addAll(currentSegment)
+                    .color(Color.BLUE)
+                    .width(20));
+            currentSegment.clear();
+            currentSegment.add(currentPoint);
+            ix++;
+        }
+
+        mMap.addPolyline(new PolylineOptions()
+                .addAll(currentSegment)
+                .color(Color.BLUE)
+                .width(20));
+
+    }
+
 }
